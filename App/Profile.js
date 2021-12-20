e3c5b9df394f5b3393f29850721717de3da151a8
Wsm2018 <40735297+Wsm2018@users.noreply.github.com>
2021-12-20 17:02:15 +0300

    Profile screen updates

--- App/Profile.js
+++ App/Profile.js
@@ -1,24 +1,124 @@
 import React, { useState, useEffect } from "react";
 import {
-  View,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-  Dimensions,
-  Alert,
-  Button,
+	View,
+	Text,
+	TouchableOpacity,
+	StyleSheet,
+	Dimensions,
+	Alert,
+	Image,
 } from "react-native";
-import { Icon, Header } from "react-native-elements";
+import { Icon, Header, Divider } from "react-native-elements";
 import db from "../db";
 import { StatusBar } from "expo-status-bar";
 import colors from "../colors.json";
 const { height, width } = Dimensions.get("screen");
 
+const settings = [
+	"Weekly Peformance",
+	"Settings",
+	"FAQ",
+	"Support",
+	"Terms and Conditions",
+];
+
 export default function Profile(props) {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Profile</Text>
-      <Button title="Logout" onPress={() => firebase.auth().signOut()} />
-    </View>
-  );
+	const [user, setUser] = useState(null);
+
+	const getUser = async () => {
+		const userRef = await db
+			.firestore()
+			.collection("Users")
+			.doc(db.auth().currentUser.uid)
+			.get();
+		const userData = userRef.data();
+		setUser(userData);
+	};
+
+	useEffect(() => {
+		getUser();
+	}, []);
+
+	return (
+		<View style={styles.container}>
+			<Header
+				containerStyle={{
+					borderBottomColor: colors.main,
+					borderBottomWidth: 1,
+				}}
+				backgroundColor={colors.main}
+				leftComponent={
+					<TouchableOpacity onPress={() => props.navigation.openDrawer()}>
+						<Icon name="menu" type="feather" color={colors.white} size={25} />
+					</TouchableOpacity>
+				}
+			/>
+			<View style={styles.topView}>
+				<Image
+					source={{ uri: user && user.profilePic }}
+					style={{ width: width / 2.5, height: height /5, borderRadius: 100 }}
+				/>
+				<Text
+					style={{
+						marginTop: "3%",
+						color: "white",
+						fontFamily: "Montserrat-Bold",
+						// fontWeight: "bold",
+						fontSize: 30,
+					}}
+				>
+					{user && user.displayName}
+				</Text>
+
+			</View>
+			<View style={styles.bottomView}>
+				{settings.map((item, index) => (
+					<TouchableOpacity style={styles.settingItem} key={index}>
+						<View style={{ flex: 3 }}>
+							<Text style={{ fontFamily: "Montserrat-SemiBold", fontSize: 18 }}>
+								{item}
+							</Text>
+						</View>
+						<View style={{ flex: 1 }}>
+							<Icon
+								name="chevron-small-right"
+								type="entypo"
+								size={30}
+								style={{ justifyContent: "flex-end" }}
+							/>
+						</View>
+					</TouchableOpacity>
+				))}
+			</View>
+
+			<StatusBar style="light" backgroundColor={colors.main} />
+		</View>
+	);
 }
+
+const styles = StyleSheet.create({
+	container: { flex: 1, backgroundColor: colors.background },
+	topView: {
+		flex: 2,
+		backgroundColor: colors.main,
+		borderBottomLeftRadius: 35,
+		borderBottomRightRadius: 35,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	bottomView: {
+		flex: 3,
+		// marginTop: "1%"
+		// justifyContent: "space-evenly",
+	},
+	settingItem: {
+		flex: 1,
+		flexDirection: "row",
+		width: width,
+		// justifyContent: "space-between",
+		// marginTop: "5%",
+		// backgroundColor:"red",
+		alignItems: "center",
+		marginLeft: "5%",
+	},
+});
