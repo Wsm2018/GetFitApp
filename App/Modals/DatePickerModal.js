30306b4d8ef695825f9314a755ea3f3b666b7593
Wsm2018 <40735297+Wsm2018@users.noreply.github.com>
2021-12-21 17:19:06 +0300

    Pushing updates
    - Working on DatePickerModal for user to select start date and end date.

--- App/Modals/DatePickerModal.js
+++ App/Modals/DatePickerModal.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import {
-  View,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-  Dimensions,
-  Alert,
-  Image,
+	View,
+	Text,
+	TouchableOpacity,
+	StyleSheet,
+	Dimensions,
+	Alert,
+	Image,
 } from "react-native";
-import { Icon, Header, Divider } from "react-native-elements";
+import { Icon, Input } from "react-native-elements";
 import { StatusBar } from "expo-status-bar";
 import colors from "../../colors.json";
 const { height, width } = Dimensions.get("screen");
@@ -17,32 +17,132 @@ import moment from "moment";
 import Modal from "react-native-modal";
 
 export default function DatePickerModal(props) {
-  const { openModal, setOpenModal } = { props };
+	const { openModal, setOpenModal, startDate, setStartDate } = props;
 
-  return (
-    <Modal
-      isVisible={openModal}
-      onSwipeComplete={() => setOpenModal(false)}
-      swipeDirection={["down"]}
-      style={styles.view}
-    >
-      {console.log("hello")}
-      <View style={styles.modal}>
-        <Text>Hello</Text>
-      </View>
-    </Modal>
-  );
+	const dateChange = (date) => {
+		console.log(date);
+	};
+
+	return (
+		<Modal
+			isVisible={openModal}
+			onSwipeComplete={() => setOpenModal(false)}
+			swipeDirection={["down"]}
+			onBackdropPress={() => setOpenModal(false)}
+			style={styles.view}
+		>
+			{/* Title Container */}
+			<View style={styles.modal}>
+				<View style={styles.titleContainer}>
+					<Text style={styles.titleStyle}>Date Range</Text>
+					<TouchableOpacity onPress={() => setOpenModal(false)}>
+						<Icon
+							name="close-circle-outline"
+							type="ionicon"
+							size={width / 12}
+							style={{ marginRight: "3%" }}
+						/>
+					</TouchableOpacity>
+				</View>
+
+				{/* Dates Container */}
+				<View
+					style={{
+						flex: 3,
+						// backgroundColor: "blue",
+						justifyContent: "space-evenly",
+						marginLeft: "3%",
+					}}
+				>
+					<View>
+						<Text style={styles.labelStyle}>Start Date:</Text>
+						<DatePicker
+							placeholder="Start Date:"
+							mode="date"
+							date={moment(startDate).format("YYYY-MM-DD")}
+							format="YYYY-MM-DD"
+							confirmBtnText="CONFIRM"
+							onDateChange={dateChange}
+							maxDate={moment().format("YYYY-MM-DD")}
+						/>
+					</View>
+
+					<View>
+						<Text style={styles.labelStyle}>End Date:</Text>
+						<DatePicker
+							placeholder="End Date:"
+							mode="date"
+							date={moment(startDate).format("YYYY-MM-DD")}
+							format="YYYY-MM-DD"
+							confirmBtnText="CONFIRM"
+							onDateChange={dateChange}
+							maxDate={moment().format("YYYY-MM-DD")}
+						/>
+					</View>
+				</View>
+
+				{/* Confirm Button Container */}
+				<View style={styles.buttonContainer}>
+					<TouchableOpacity style={styles.buttonStyle}>
+						<Icon
+							name="checkmark-circle-outline"
+							type="ionicon"
+							size={width / 14}
+							color={colors.main}
+							style={{ marginRight: "3%" }}
+						/>
+						<Text style={styles.confirmTextStyle}>Confirm</Text>
+					</TouchableOpacity>
+				</View>
+			</View>
+		</Modal>
+	);
 }
 
 const styles = StyleSheet.create({
-  view: {
-    justifyContent: "flex-end",
-    margin: 0,
-  },
-  modal: {
-    flex: 1,
-    backgroundColor: colors.rest,
-    height: height,
-    width: width,
-  },
+	view: {
+		justifyContent: "flex-end",
+		margin: 0,
+		// height: height / 3,
+	},
+	modal: {
+		flex: 0.6,
+		backgroundColor: "white",
+		borderTopLeftRadius: 30,
+		borderTopRightRadius: 30,
+	},
+	buttonContainer: { flex: 1, justifyContent: "center", alignItems: "center" },
+	buttonStyle: {
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		borderWidth: 1,
+		borderStyle: "solid",
+		borderColor: colors.main,
+		width: "80%",
+		height: "65%",
+		borderRadius: 10,
+	},
+	confirmTextStyle: {
+		fontSize: 18,
+		fontFamily: "Montserrat-SemiBold",
+		color: colors.main,
+		textTransform: "uppercase",
+	},
+	titleStyle: {
+		fontSize: 20,
+		fontFamily: "Montserrat-SemiBold",
+		marginLeft: "3%",
+		color: "black",
+	},
+	titleContainer: {
+		flex: 0.75,
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+  labelStyle: {
+    fontFamily: "Montserrat-Medium",
+    fontSize: 20
+  }
 });
--- App/Performance.js
+++ App/Performance.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import {
-  View,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-  Dimensions,
-  Alert,
-  Image,
+	View,
+	Text,
+	TouchableOpacity,
+	StyleSheet,
+	Dimensions,
+	Alert,
+	Image,
 } from "react-native";
 import { Icon, Header, Divider } from "react-native-elements";
 import db from "../db";
@@ -18,70 +18,77 @@ import moment from "moment";
 import DatePickerModal from "./Modals/DatePickerModal";
 
 export default function Peformance(props) {
-  const [startDate, setStartDate] = useState(moment().format("MMM DD, YYYY"));
-  const [endDate, setEndDate] = useState(moment().format("MMM DD, YYYY"));
-  const [openModal, setOpenModal] = useState(false);
+	const [startDate, setStartDate] = useState(moment().format("MMM DD, YYYY"));
+	const [endDate, setEndDate] = useState(moment().format("MMM DD, YYYY"));
+	const [openModal, setOpenModal] = useState(false);
 
-  return (
-    <View style={styles.container}>
-      <Header
-        containerStyle={{
-          borderBottomColor: colors.main,
-          borderBottomWidth: 1,
-        }}
-        backgroundColor={colors.main}
-        leftComponent={
-          <TouchableOpacity onPress={() => props.navigation.goBack()}>
-            <Icon
-              name="arrow-back"
-              type="ionicon"
-              color={colors.white}
-              size={25}
-            />
-          </TouchableOpacity>
-        }
-        centerComponent={<Text style={styles.textStyle}>Performance</Text>}
-      />
-      <View style={styles.topView}>
-        <Text style={styles.dateText}>{`${startDate} - ${endDate}`}</Text>
-        <TouchableOpacity
-          style={{ marginRight: "5%" }}
-          onPress={() => setOpenModal(!openModal)}
-        >
-          <Icon name="filter" type="font-awesome" size={25} />
-        </TouchableOpacity>
-      </View>
-      <View style={styles.bottomView}></View>
-      <DatePickerModal openModal={openModal} setOpenModal={setOpenModal} />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Header
+				containerStyle={{
+					borderBottomColor: colors.main,
+					borderBottomWidth: 1,
+				}}
+				backgroundColor={colors.main}
+				leftComponent={
+					<TouchableOpacity onPress={() => props.navigation.goBack()}>
+						<Icon
+							name="arrow-back"
+							type="ionicon"
+							color={colors.white}
+							size={25}
+						/>
+					</TouchableOpacity>
+				}
+				centerComponent={<Text style={styles.textStyle}>Performance</Text>}
+			/>
+			<View style={styles.topView}>
+				<Text style={styles.dateText}>{`${startDate} - ${endDate}`}</Text>
+				<TouchableOpacity
+					style={{ marginRight: "5%" }}
+					onPress={() => setOpenModal(!openModal)}
+				>
+					<Icon name="filter" type="font-awesome" size={25} />
+				</TouchableOpacity>
+			</View>
+			<View style={styles.bottomView}></View>
+			<DatePickerModal
+				openModal={openModal}
+				setOpenModal={setOpenModal}
+				startDate={startDate}
+				setStartDate={setStartDate}
+        endDate={endDate}
+        setEndDate={setEndDate}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.background,
-  },
-  textStyle: {
-    fontFamily: "Montserrat-Medium",
-    color: colors.white,
-    fontSize: 20,
-  },
-  topView: {
-    flex: 1,
-    backgroundColor: colors.homeExer1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  bottomView: {
-    flex: 7,
-    backgroundColor: colors.homeExer2,
-  },
-  dateText: {
-    color: "black",
-    fontFamily: "Montserrat-SemiBold",
-    fontSize: 20,
-    marginLeft: "5%",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: colors.background,
+	},
+	textStyle: {
+		fontFamily: "Montserrat-Medium",
+		color: colors.white,
+		fontSize: 20,
+	},
+	topView: {
+		flex: 1,
+		backgroundColor: colors.homeExer1,
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+	bottomView: {
+		flex: 7,
+		backgroundColor: colors.homeExer2,
+	},
+	dateText: {
+		color: "black",
+		fontFamily: "Montserrat-SemiBold",
+		fontSize: 20,
+		marginLeft: "5%",
+	},
 });
